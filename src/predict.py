a5ba1ac9b0ffe4ad6fc4dea4bd9b10b6214d5282
Colin Van Hoveln <colin_vanhoveln@outlook.com>
2026-02-19 16:25:54 -0500

    added a dataframe as an example input into the prediction to avoid error

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,18 @@
 import joblib
+import pandas as pd
 
 model = joblib.load("models/model.pkl")
-example = [[8.3252, 41.0, 6.984127, 1.0238095, 322.0, 2.555556, 37.88, -122.23]]
-prediction = model.predict(example)
+custom_house = pd.DataFrame(
+    {
+        "MedInc": [3.5],
+        "HouseAge": [20],
+        "AveRooms": [5],
+        "AveBedrms": [1],
+        "Population": [1000],
+        "AveOccup": [3],
+        "Latitude": [34.05],
+        "Longitude": [-118.25],
+    }
+)
+prediction = model.predict(custom_house)
 print(f"Prediction: {prediction}")
